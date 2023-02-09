7c4dbbef5d151615b2366ed2cb01bb60af9b0c25
connor-eg <connoregarcia@gmail.com>
2023-02-08 19:16:06 -0600

    Migrated password to an integer (supporting hashing operations) and converted this class to a record.

--- src/main/java/com/revature/model/Employee.java
+++ src/main/java/com/revature/model/Employee.java
@@ -1,57 +1,33 @@
 package com.revature.model;
 
-public class Employee {
-    //Information about the employee.
-    // These may be public because they are finalized when this class is instantiated.
-    private final String email; //This employee's email address
-    private final String password; //This employee's password
-    private final Roles role; //This employee's role (this is used to determine their permissions)
-    private final int id; //This employee's ID in the database (this is mostly unused)
-
-    public Employee(String email, String password, Roles role, int id) {
+/**
+ * @param email    Information about the employee. These may be public because they are finalized when this class is instantiated.This employee's email address
+ * @param password This employee's password
+ * @param role     This employee's role (this is used to determine their permissions)
+ * @param id       This employee's ID in the database (this is mostly unused)
+ */
+public record Employee(String email, int password, com.revature.model.Employee.Roles role, int id) {
+    public Employee {
         //Validation
         if (email == null) {
             throw new IllegalStateException("The email of an employee may not be null");
         }
-        if (password == null) {
-            throw new IllegalStateException("The password of an employee may not be null");
-        }
         if (role == null) {
             throw new IllegalStateException("The role of an employee may not be null (try Employee.Roles.STANDARD)");
         }
-        if(id < 1){
+        if (id < 1) {
             throw new IllegalStateException("The ID of an employee must be at least 1");
         }
 
         //Initialization
-        this.email = email;
-        this.password = password;
-        this.role = role;
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public Roles getRole() {
-        return role;
-    }
-
-    public int getId() {
-        return id;
     }
 
     @Override
     public String toString() {
         return "Employee{" +
-                "email='" + getEmail() + '\'' +
-                ", password='" + getPassword() + '\'' +
-                ", role=" + getRole() +
+                "email='" + email() + '\'' +
+                ", password='" + password() + '\'' +
+                ", role=" + role() +
                 '}';
     }
 
